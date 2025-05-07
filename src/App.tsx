2a6a4bba7b6d8a2f67d4c36c6a2f58af5cac3e04
ArielChaves <139404966+ArielChaves@users.noreply.github.com>
2025-05-06 23:10:09 -0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-    // Importação de componentes e estilos globais
-    import Header from './components/Cabecalho'
-    import Hero from './components/Hero'
-    import ListaVagas from './containers/ListaVagas'
-    import './global.css'
+  import Header from './components/Cabecalho'
+import Hero from './components/Hero'
+import ListaVagas from './containers/ListaVagas'
 
-    // Componente App principal da aplicação
-    function App() {
-    return (
-        <>
-        {/* Renderização do componente Header, responsável pelo cabeçalho do site */}
-        <Header />
-        {/* Renderização do componente Hero, responsável pela seção herói (banner principal) */}
-        <Hero />
-        {/* Div container para centralizar e estilizar o componente ListaVagas */}
-        <div className="container">
-            {/* Renderização do componente ListaVagas, responsável por exibir a lista de vagas de emprego */}
-            <ListaVagas />
-        </div>
-        </>
-    )
-    }
+import './global.css'
 
-    // Exportação do componente App para ser utilizado em outros lugares da aplicação
-    export default App
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <div className="container">
+        <ListaVagas />
+      </div>
+    </>
+  )
+}
+
+export default App
