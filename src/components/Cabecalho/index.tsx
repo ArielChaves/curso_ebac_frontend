2985a295c4fbb8be7b5154d141e82d02ebebb56d
dafontebfp <bruniko.pereira@gmail.com>
2025-05-14 20:21:08 -0300

    Final project

---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,9 @@
+import Cabecalho from './Styles'
+
+const Header = () => (
+  <Cabecalho>
+    <h1>EBAC Jobs</h1>
+  </Cabecalho>
+)
+
+export default Header
---
+++ src/components/FormVagas/index.tsx
@@ -0,0 +1,35 @@
+import React, { useState, FormEvent } from 'react'
+import { StyledForm, BtnPesquisar } from './Styles' // Agora está correto!
+import styled from 'styled-components'
+
+const Input = styled.input`
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`
+
+interface Props {
+  aoPesquisar: (termo: string) => void
+}
+
+const FormVagas = ({ aoPesquisar }: Props) => {
+  const [termo, setTermo] = useState<string>('')
+
+  const aoEnviarForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    aoPesquisar(termo.toLowerCase())
+  }
+
+  return (
+    <StyledForm onSubmit={aoEnviarForm}>
+      <Input
+        placeholder="Front-end, fullstack, node, design"
+        onChange={(e) => setTermo(e.target.value)}
+        type="search"
+      />
+      <BtnPesquisar type="submit">Pesquisar</BtnPesquisar>
+    </StyledForm>
+  )
+}
+
+export default FormVagas
